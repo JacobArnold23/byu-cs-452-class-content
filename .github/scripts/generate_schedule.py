186b976a8ad6d47b4bdae96bb765260e11b61dfd
WickedWicket373 <jsarnold373@gmail.com>
2026-02-05 11:28:17 -0700

    up to aisql

--- .github/scripts/generate_schedule.py
+++ .github/scripts/generate_schedule.py
@@ -24,8 +24,8 @@ README_PATH = "README.md"
 MODULE_DAYS = {
     # examples
     "01-sql": [1,2,3,4,5,6,7],
-    "02-erd-examples": [5,6,7,8,9,10],
-    "03-functions": [3,4,5],
+    "02-erd-examples": [5,6,7,8,9,10,11],
+    "03-ai-sql": [11,12],
 }
 
 # ======================
